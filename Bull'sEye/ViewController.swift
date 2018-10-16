08356572ed49bc0def8fa75833f6ef05a518293c
Aracely Chavez <aracelychavez@aracelys-imac.local>
2018-10-16 15:11:56 -0500

    first commit message

--- Bull'sEye/ViewController.swift
+++ Bull'sEye/ViewController.swift
@@ -14,7 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func showAlert() {
+        
+        let alert = UIAlertController(title: "Hello World!", message: "This is my frist app!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert,animated: true, completion: nil)
+    }
+    
+    @IBAction func KnockKnock() {
+        
+        let alert = UIAlertController(title: "knock knock!", message: "Who's There!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "little old lady", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert,animated: true, completion: nil)
+    }
+    
 }
+
+
 
